e6beb85ac65de6a589cc18a04c15ca30bcce26f0
ignas <ignasjas3@gmail.com>
2017-02-18 19:07:06 +0200

    project upload

--- EmploeeApp/App_Start/RouteConfig.cs
+++ EmploeeApp/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace EmploeeApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Emploee", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- EmploeeApp/Global.asax.cs
+++ EmploeeApp/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmploeeApp.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,14 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+
 namespace EmploeeApp
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
